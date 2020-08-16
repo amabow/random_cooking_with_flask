bb6d7a9028dc320ba1551cef9b7be6a06f8bc88e
amabow <55275440+amabow@users.noreply.github.com>
2020-08-16 13:20:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.utils import secure_filename
 from flask_migrate import Migrate
 
 import random
